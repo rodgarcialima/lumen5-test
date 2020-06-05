11ec63be31a33465612f56a33f025db9a7128624
Rod Lima <rodgarcialima@gmail.com>
2020-06-05 16:59:16 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,145 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useRef, useReducer, useContext } from 'react';
 import './App.css';
+import { stringToColour } from './colorUtil';
+
+const Todo = ({ todo }) => {
+  const dispatch = useContext(TodosDispatch);
+  const handleTodoClick = () => {
+    dispatch({ type: 'CHECK_UNCHECK', todo });
+  };
+  return (
+    <div className="todo-item">
+      <input type="checkbox" checked={todo.done} onChange={handleTodoClick} />
+      <span 
+        className="todo-task" 
+        onClick={handleTodoClick} 
+        style={{ textDecoration: todo.done ? "line-through" : "" }}
+      >
+        {todo.id}
+      </span>
+      <span 
+        className="todo-category"
+        onClick={handleTodoClick} 
+        style={{
+          backgroundColor: stringToColour(todo.category),
+          textDecoration: todo.done ? "line-through" : ""
+        }}
+      >
+        {todo.category}
+      </span>
+    </div>
+  );
+};
+
+const filterTodos = (allTodoItems, selectedCategory) => {
+  const filtered = {};
+  Object.keys(allTodoItems).forEach(id => {
+    if (!selectedCategory || allTodoItems[id].category === selectedCategory) {
+      filtered[id] = { ...allTodoItems[id] };
+    }
+  });
+  return filtered;
+};
+
+const TodoList = ({ todos, selectedCategory }) => {
+  const filteredList = filterTodos(todos, selectedCategory);
+  return (
+    <div className="todo-list">
+      <span className="todo-title">TODO</span>
+      {Object.keys(filteredList).map((id, idx) => {
+        return <Todo key={idx} todo={{ id, ...todos[id] }}/>
+      })}
+    </div>
+  );
+}
+
+const TodoForm = () => {
+  const taskInputRef = useRef(null);
+  const dispatch = useContext(TodosDispatch);
+  const [todo, setTodo] = useState({ id: '', category: '' });
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    if (!todo.id || !todo.category) return;
+    dispatch({ type: 'ADD', todo });
+    setTodo({ id: '', category: '' });
+    taskInputRef.current.focus();
+  };
+
+  const updateFormField = event => {
+    setTodo({
+      ...todo,
+      [event.target.name]: event.target.value
+    });
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="todo-form">
+      <button type="submit">+</button>
+      <input type="text" value={todo.id} name="id" onChange={updateFormField} ref={taskInputRef} />
+      <input type="text" value={todo.category} name="category" onChange={updateFormField} />
+    </form>
+  );
+};
+
+const TodoFilter = ({ todos, category, handleCategoryChange }) => {
+  const categories = Array.from(new Set([...Object.keys(todos).map(key => todos[key].category)]));
+  return (
+    <>
+      <div>Categories</div>
+      <select onChange={handleCategoryChange} value={category}>
+        <option value="">All</option>
+        {categories.map((category, idx) => 
+          <option key={idx} value={category}>{category}</option>
+        )}
+      </select>
+    </>
+  );
+}
+
+const TodosDispatch = React.createContext(null);
+
+const todosReducer = (state, action) => {
+  const { id, category, done } = action.todo;
+  switch (action.type) {
+    case 'ADD':
+      return { ...state, [id]: { category, done: false } };
+    case 'CHECK_UNCHECK':
+      return { ...state, [id]: { category, done: !done } };
+    default:
+      throw new Error();
+  }
+}
+
+const initialState = {
+  'laundry': { category: 'home', done: false },
+  'write report': { category: 'work', done: true },
+  'vacuum': { category: 'home', done: false }
+};
 
 function App() {
+  const [todos, dispatch] = useReducer(todosReducer, initialState);
+  const [category, setCategory] = useState('');
+
+  const handleCategoryChange = event => {
+    console.log(event.target.value);
+    setCategory(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="todo-container">
+        <TodosDispatch.Provider value={dispatch}>
+          <TodoList todos={todos} selectedCategory={category} />
+          <TodoForm />
+        </TodosDispatch.Provider>
+      </div>
+      <div className="combo-container">
+          <TodoFilter todos={todos} category={category} handleCategoryChange={handleCategoryChange} />
+      </div>
     </div>
   );
 }
 
 export default App;
+
--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,36 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('filterTodos', function() {
+  // const todos = {
+  //   'laundry': { category: 'home', done: false },
+  //   'write report': { category: 'work', done: true },
+  //   'vacuum': { category: 'home', done: false }
+  // };
+
+  // it('should return all elements if null category is passed', function() {
+  //   const actualResult = filterTodos(todos, null);
+  //   expect(actualResult).toEqual(todos);
+  // });
+
+  // it('should return all todos if no category is passed', function() {
+  //     const actualResult = filterTodos(todos, '');
+  //     expect(actualResult).toEqual(todos);
+  // });
+
+  // it('should return 2 elements if home category is passed', function() {
+  //   const actualResult = filterTodos(todos, 'home');
+  //   const expectedResult = {
+  //     'laundry': { category: 'home', done: false },
+  //     'vacuum': { category: 'home', done: false }
+  //   };
+  //   expect(actualResult).toEqual(expectedResult);
+  // });
+
+  // it('should return nothing if category does not exists', function() {
+  //   const actualResult = filterTodos(todos, 'does not exists');
+  //   const expectedResult = {};
+  //   expect(actualResult).toEqual(expectedResult);
+  // });
 });
+
